18e30279033be0ae328427db8a71129061f33d63
Patrycja147 <56259388+Patrycja147@users.noreply.github.com>
2019-10-21 14:13:37 +0200

    Add files via upload

---
+++ 02-ControlStructures/02.12.py
@@ -0,0 +1,6 @@
+x=int(input("Podaj liczbę x: "))
+y=int(input("Podaj liczbę y: "))
+if x>=0 and y>=0:
+    print("Żadna z liczb nie jest ujemna")
+else:
+    print("Przynajmniej jedna z liczb jest ujemna")
