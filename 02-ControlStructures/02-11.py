d5b61903130177a6fc10852cc439e8759f7edc3d
Patrycja147 <56259388+Patrycja147@users.noreply.github.com>
2019-10-21 14:05:53 +0200

    Add files via upload

---
+++ 02-ControlStructures/02-11.py
@@ -0,0 +1,8 @@
+login="marek"
+hasło="m-123"
+x=input("Podaj login: ")
+y=input("Podaj hasło: ")
+if x!=login or y!=hasło:
+    print("Podane dane są niepoprawne.")
+else:
+    print("Podane dane są prawidłowe")
