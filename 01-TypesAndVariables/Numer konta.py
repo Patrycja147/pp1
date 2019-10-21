bf27d8f025f9f41d47f5183385fb7308a4a8ebc9
Patrycja147 <56259388+Patrycja147@users.noreply.github.com>
2019-10-21 11:12:38 +0200

    Add files via upload

---
+++ 01-TypesAndVariables/Numer konta.py
@@ -0,0 +1,2 @@
+Numer = input("Podaj nr rachunku bankowego: ")
+print("Numer rachunku:", Numer[:2],Numer[2:6],Numer[6:10],Numer[10:14],Numer[14:18],Numer[18:22],Numer[22:26])
---
+++ 01-TypesAndVariables/Wzrost IM.py
@@ -0,0 +1,4 @@
+Wzrost=163
+Stopa=5
+Cal=35
+print("Mam",Wzrost,"cm wzrostu, tj",Stopa, "stóp i",Cal,"cali.")
