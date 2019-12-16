713c51ea4d65e2c1eaa56742fffb8c6ea1ac61d6
s-115-35 <s-115-35@gumiforest.ae.krakow.pl>
2019-12-16 14:41:23 +0100

    16.12.2019

--- 08-DataStructures/MyMatrix.py
+++ 08-DataStructures/MyMatrix.py
@@ -2,21 +2,31 @@ class matrix():
 
     @staticmethod
     def create(x,y):
-        matrix = []
-        value = 0
-        for i in range(x):
-            # create single row
-            row = []
-            for j in range(y):
-                row.append(value)
-            # add row to matrix
-            matrix.append(row)
+        matrix=[[0 for i in range(x)] for j in range(y)]
         return matrix
+    
+    @staticmethod
+    def create_unit(x):
+        z=matrix.create(x,x)
+        a=0
+        for i in z:
+            i[a]=1
+            a+=1
+        return z
+    
+    @staticmethod    
+    def fill_random(matrix,b,n):
+        import random
+        for i in matrix:
+            for j in range(len(i)):
+                i[j]=random.randint(b,n)
+
 
     @staticmethod
     def print(matrix):
         for row in matrix:
             print(row)
 
-m = matrix.create(4,3)
+m=matrix.create(4,5)
+matrix.fill_random(m,1,8)
 matrix.print(m)
